e95d10de3dd9f352fcf9c70d1bb25a9aacacc1ad
goinhacker <goinhacker@gmail.com>
2017-06-09 00:03:52 +0900

    Add Java HelloWorld

---
+++ java/src/main/java/HelloWorld.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class HelloWorld {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int cases = sc.nextInt();
+        for (int i = 0; i < cases; i++) {
+            String name = sc.next();
+            System.out.println("Hello, " + name + "!");
+        }
+    }
+}
