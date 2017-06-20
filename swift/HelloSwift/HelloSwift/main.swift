91afe8d8a0139ff6d9b7103cd5d8e52320b97bec
Moonbeom KWON <MBKWON@WMP-2016090004.local>
2017-06-20 16:06:59 +0900

    Add Swift HelloWorld

---
+++ swift/HelloSwift/HelloSwift/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  HelloSwift
+//
+//  Created by Moonbeom KWON on 2017. 6. 20..
+//  Copyright © 2017년 mbkyle. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+let response = readLine(strippingNewline: true)
+
+if let text = response {
+    print( text )
+}
