6f26be1d36e8f5ddf7fcbfcfc84eacc1b4758c33
rosd89 <zkdlffpem@naver.com>
2017-07-19 02:06:37 +0900

    HelloWorld [js]: 문법 수정
    function -> arrow function으로 변경
    변수 키워드 수정 var -> const, let

--- javascript/src/scripts/HelloWorld.js
+++ javascript/src/scripts/HelloWorld.js
@@ -1,11 +1,9 @@
-
-var input = [];
+const input = [];
 
 require('readline')
-.createInterface(process.stdin, process.stdout)
-.on('line', function(line) {
-  input.push(line.trim());
-}).on('close', function() {
-  for (var i = 0; i < input.length; i++)
-    console.log('Hello, ' + input[i] + '!');
-});
+  .createInterface(process.stdin, process.stdout)
+  .on('line', line => input.push(line.trim()))
+  .on('close', _ => {
+    for (let i = 0; i < input.length; i++)
+      console.log('Hello, ' + input[i] + '!');
+  });
