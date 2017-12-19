6b96ad40a209fe226e1bf006efbd2fb00a11d27e
Lazysoul <myeonginwoo@gmail.com>
2017-12-19 21:29:48 +0900

    Solbed SimpleBalancedParentheses

---
+++ kotlin/src/main/kotlin/SimpleBalancedParentheses.kt
@@ -0,0 +1,32 @@
+object SimpleBalancedParentheses {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val positive1 = "(()()()())"
+        val positive2 = "(((())))"
+        val positive3 = "(()((())()))"
+
+        val negative1 = "((((((())"
+        val negative2 = "()))"
+        val negative3 = "(()()(()"
+
+        val bonus1 = "(5+6)∗(7+8)/(4+3)"
+
+        require(solve(positive1.toList(), listOf()))
+        require(solve(positive2.toList(), listOf()))
+        require(solve(positive3.toList(), listOf()))
+
+        require(!solve(negative1.toList(), listOf()))
+        require(!solve(negative2.toList(), listOf()))
+        require(!solve(negative3.toList(), listOf()))
+
+        require(solve(bonus1.toList(), listOf()))
+    }
+
+    private fun solve(list: List<Char>, acc: List<Char>): Boolean = when {
+        list.isEmpty() -> acc.isEmpty()
+        list.first() == ')' && acc.isEmpty() -> false
+        list.first() == '(' -> solve(list.drop(1), acc.plus(list.first()))
+        list.first() == ')' -> solve(list.drop(1), acc.dropLast(1))
+        else -> solve(list.drop(1), acc)
+    }
+}
