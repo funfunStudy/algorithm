b02212d4f91f6b30971a1ce4cf354a82c343aa40
조재용 <jaeyong83.cho@sk.com>
2017-12-19 21:56:52 +0900

    SimpleBalancedParentheses Haskell

--- haskell/src/main/haskell/simpleBalancedParentheses/Main.hs
+++ haskell/src/main/haskell/simpleBalancedParentheses/Main.hs
@@ -3,14 +3,10 @@ module Main where
 main :: IO ()
 main = do
     line <- getLine
-    putStrLn $ boolToString $ checkBalance line 0
+    putStrLn $ if checkBalance line 0 then "TRUE" else "FALSE"
 
 checkBalance :: String -> Int -> Bool
 checkBalance [] n = 0 == n
 checkBalance (x:xs) n
     | x == '(' = checkBalance xs (n + 1)
     | otherwise = checkBalance xs (n - 1)
-
-boolToString :: Bool -> String
-boolToString True = "TRUE"
-boolToString False = "FALSE"
