90c7588bd0700ba68d1189293408f49d8a5401dd
조재용 <jaeyong83.cho@sk.com>
2017-12-19 22:16:51 +0900

    SimpleBalancedParentheses Haskell

--- haskell/src/main/haskell/simpleBalancedParentheses/Main.hs
+++ haskell/src/main/haskell/simpleBalancedParentheses/Main.hs
@@ -7,6 +7,7 @@ main = do
 
 checkBalance :: String -> Int -> Bool
 checkBalance [] n = 0 == n
+checkBalance [x] n = if 0 > n then False else True
 checkBalance (x:xs) n
     | x == '(' = checkBalance xs (n + 1)
     | otherwise = checkBalance xs (n - 1)
