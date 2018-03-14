a53a99a65551207fb84189e4c7ee20ad08c26fed
Lazysoul <myeonginwoo@gmail.com>
2018-03-14 21:45:42 +0900

    solved SugarDelivery
    myeongin with Leesm

--- kotlin/src/main/kotlin/SugarDelivery.kt
+++ kotlin/src/main/kotlin/SugarDelivery.kt
@@ -1,30 +1,26 @@
-fun main(args: Array<String>) {
+object SugarDelivery {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        require(solve(18) == 4, { "error" })
+        require(solve(4) == -1, { "error" })
+        require(solve(6) == 2, { "error" })
+        require(solve(9) == 3, { "error" })
+        require(solve(11) == 3, { "error" })
+        require(solve(5000) == 1000, { "error" })
+    }
 
-    require(solve(18) == 4, { "error" })
-    require(solve(4) == -1, { "error" })
-    require(solve(6) == 2, { "error" })
-    require(solve(9) == 3, { "error" })
-    require(solve(11) == 3, { "error" })
-    require(solve(5000) == 1000, { "error" })
-}
+    private fun solve(number: Int): Int {
+        return recursive(number, (number / 5))
+    }
 
-private fun solve(number: Int): Int {
-    val fiveModResult: Int = (number / 5)
-    return recursive(number, fiveModResult)
-}
-
-private tailrec fun recursive(number: Int, fiveModResult: Int): Int {
-    println("$fiveModResult, $, ${(number - (fiveModResult * 5)) / 3}")
-    val threeModResult2 = (number - (fiveModResult * 5)) / 3
-    return when {
+    private tailrec fun recursive(number: Int, fiveModResult: Int): Int = when {
         number % 5 == 0 -> fiveModResult
-        fiveModResult >= 0 -> {
-            if ((number - (fiveModResult * 5)) % 3 == 0) {
-                fiveModResult + threeModResult2
-            } else {
-                recursive(number, fiveModResult - 1)
-            }
+        fiveModResult >= 0 -> if ((number - (fiveModResult * 5)) % 3 == 0) {
+            fiveModResult + (number - (fiveModResult * 5)) / 3
+        } else {
+            recursive(number, fiveModResult - 1)
         }
         else -> -1
     }
 }
+
